b524705a91fa5a2b6c8456991a3c113b941f1db8
aibees <aibees1129@naver.com>
2022-06-13 18:33:26 +0900

    port 변경

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@@': path.resolve(__dirname, './public/sass')
+      '@@': path.resolve(__dirname, './public/sass'),
+      'PROPS': path.resolve(__dirname, './public/properties') 
     }
   },
   server: {
     host: '0.0.0.0',
-    port: 7077
+    port: 18900
   },
   plugins: [vue()]
 })
